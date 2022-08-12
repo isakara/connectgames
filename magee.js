0782de081c10fa0c309a7c067a3230feac245db3
Leigh Magee <isakara@gmail.com>
2022-08-12 11:01:49 +0930

    updated magee.js
    added end state, loan payback mechanics, banking mechanics partially implemented (interest and depositing only)

--- magee.js
+++ magee.js
@@ -7,8 +7,11 @@ let cash = 2000;
 let bank = 0;
 let debt = 5000;
 let health = 100;
-let pockets = 0;
-let maxPockets = 30;
+let maxHealth = 100;
+let space = 30;
+let maxSpace = 30;
+let product = [];
+let heldProduct = [0,0,0,0,0,0];
 
 let location = ['Berlin', 'London', 'Miami ', 'Sydney', 'Tokyo ', 'Moscow'];
 let city = location[0];
@@ -31,12 +34,13 @@ function onConnect()
 function setPrices()
 {
 	doDebt();
-	product1 = rndPrice(10, 60);
-	product2 = rndPrice(70, 250);
-	product3 = rndPrice(300, 900);
-	product4 = rndPrice(1000, 4500);
-	product5 = rndPrice(5000, 14000);
-	product6 = rndPrice(15000, 30000);
+	bankCalc();
+	product[0] = rndPrice(10, 60);
+	product[1] = rndPrice(70, 250);
+	product[2] = rndPrice(300, 900);
+	product[3] = rndPrice(1000, 4500);
+	product[4] = rndPrice(5000, 14000);
+	product[5] = rndPrice(15000, 30000);
 }
 
 function drawTravel()
@@ -65,7 +69,7 @@ function drawStatus()
 	drawBox(12, 16, 0, 24, 8);
 	drawText(city, 12, 18, 1)
 	drawText('Day: ' + turn + '\/' + maxTurn, 12, 29, 1);
-	drawText("Health: " + health, 12, 18, 3);
+	drawText("Health: " + health + '\/' + maxHealth, 12, 18, 3);
 	drawText("  Cash: " + cash, 12, 18, 4);
 	drawText("  Bank: " + bank, 12, 18, 5);
 	drawText("  Debt: " + debt, 12, 18, 6);
@@ -74,25 +78,32 @@ function drawStatus()
 function drawInventory()
 {
 	drawBox(12, 2, 8, 25, 12);
-	drawText("INVENTORY", 12, 9, 9);
+	drawText("INVENTORY", 12, 10, 9);
+	drawText("   Product 1 - " + heldProduct[0], 12, 3, 11);
+	drawText("   Product 2 - " + heldProduct[1], 12, 3, 12);
+	drawText("   Product 3 - " + heldProduct[2], 12, 3, 13);
+	drawText("   Product 4 - " + heldProduct[3], 12, 3, 14);
+	drawText("   Product 5 - " + heldProduct[4], 12, 3, 15);
+	drawText("   Product 6 - " + heldProduct[5], 12, 3, 16);
+	drawText("Space: " + space + "\/" + maxSpace, 12, 8, 18);
 }
 
 function drawSales()
 {
 	drawBox(12, 29, 8, 25, 12);
-	drawText("DEALER OFFERS", 12, 34, 9);
+	drawText("DEALER OFFERS", 12, 35, 9);
 	drawText("     ", 12, 42, 11);
 	drawText("     ", 12, 42, 12);
 	drawText("     ", 12, 42, 13);
 	drawText("     ", 12, 42, 14);
 	drawText("     ", 12, 42, 15);
 	drawText("     ", 12, 42, 16);
-	drawText("   Product 1 - " + product1, 12, 30, 11);
-	drawText("   Product 2 - " + product2, 12, 30, 12);
-	drawText("   Product 3 - " + product3, 12, 30, 13);
-	drawText("   Product 4 - " + product4, 12, 30, 14);
-	drawText("   Product 5 - " + product5, 12, 30, 15);
-	drawText("   Product 6 - " + product6, 12, 30, 16);
+	drawText("   Product 1 - " + product[0], 12, 30, 11);
+	drawText("   Product 2 - " + product[1], 12, 30, 12);
+	drawText("   Product 3 - " + product[2], 12, 30, 13);
+	drawText("   Product 4 - " + product[3], 12, 30, 14);
+	drawText("   Product 5 - " + product[4], 12, 30, 15);
+	drawText("   Product 6 - " + product[5], 12, 30, 16);
 	drawText("  (Z) Buy    (X) Sell", 12, 30, 18);
 }
 
@@ -126,29 +137,37 @@ function onUpdate()
 		case 7:
 			drawEnd();
 			break;
+		case 8:
+			drawBankDeposit();
+			break;
+		case 9:
+			drawBankWithdrawl();
 	}
+	if (turn > 30) {
+		room = 7;
+    }
 }
 
 function drawTitle() {
-	drawText("████████▄     ▄████████    ▄████████  ▄█      ", 12, 1, 1);
-	drawText("███   ▀███   ███    ███   ███    ███ ███      ", 12, 1, 2);
-	drawText("███    ███   ███    █▀    ███    ███ ███      ", 12, 1, 3);
-	drawText("███    ███  ▄███▄▄▄       ███    ███ ███      ", 12, 1, 4);
-	drawText("███    ███ ▀▀███▀▀▀     ▀███████████ ███      ", 12, 1, 5);
-	drawText("███    ███   ███    █▄    ███    ███ ███      ", 12, 1, 6);
-	drawText("███   ▄███   ███    ███   ███    ███ ███▌    ▄", 12, 1, 7);
-	drawText("████████▀    ██████████   ███    █▀  █████▄▄██", 12, 1, 8);
-	drawText("                                     ▀        ", 12, 1, 9);
-	drawText("     ▄█     █▄     ▄████████    ▄████████    ▄████████ ", 12, 1, 10);
-	drawText("    ███     ███   ███    ███   ███    ███   ███    ███ ", 12, 1, 11);
-	drawText("    ███     ███   ███    ███   ███    ███   ███    █▀  ", 12, 1, 12);
-	drawText("    ███     ███   ███    ███  ▄███▄▄▄▄██▀   ███        ", 12, 1, 13);
-	drawText("    ███     ███ ▀███████████ ▀▀███▀▀▀▀▀   ▀███████████ ", 12, 1, 14);
-	drawText("    ███     ███   ███    ███ ▀███████████          ███ ", 12, 1, 15);
-	drawText("    ███ ▄█▄ ███   ███    ███   ███    ███    ▄█    ███ ", 12, 1, 16);
-	drawText("     ▀███▀███▀    ███    █▀    ███    ███  ▄████████▀  ", 12, 1, 17);
-	drawText("                               ███    ███              ", 12, 1, 18);
-
+	drawText("████████▄     ▄████████    ▄████████  ▄█      ", 9, 1, 1);
+	drawText("███   ▀███   ███    ███   ███    ███ ███      ", 9, 1, 2);
+	drawText("███    ███   ███    █▀    ███    ███ ███      ", 9, 1, 3);
+	drawText("███    ███  ▄███▄▄▄       ███    ███ ███      ", 9, 1, 4);
+	drawText("███    ███ ▀▀███▀▀▀     ▀███████████ ███      ", 9, 1, 5);
+	drawText("███    ███   ███    █▄    ███    ███ ███      ", 9, 1, 6);
+	drawText("███   ▄███   ███    ███   ███    ███ ███▌    ▄", 9, 1, 7);
+	drawText("████████▀    ██████████   ███    █▀  █████▄▄██", 9, 1, 8);
+	drawText("                                     ▀        ", 9, 1, 9);
+	drawText("     ▄█     █▄     ▄████████    ▄████████    ▄████████ ", 9, 1, 10);
+	drawText("    ███     ███   ███    ███   ███    ███   ███    ███ ", 9, 1, 11);
+	drawText("    ███     ███   ███    ███   ███    ███   ███    █▀  ", 9, 1, 12);
+	drawText("    ███     ███   ███    ███  ▄███▄▄▄▄██▀   ███        ", 9, 1, 13);
+	drawText("    ███     ███ ▀███████████ ▀▀███▀▀▀▀▀   ▀███████████ ", 9, 1, 14);
+	drawText("    ███     ███   ███    ███ ▀███████████          ███ ", 9, 1, 15);
+	drawText("    ███ ▄█▄ ███   ███    ███   ███    ███    ▄█    ███ ", 9, 1, 16);
+	drawText("     ▀███▀███▀    ███    █▀    ███    ███  ▄████████▀  ", 9, 1, 17);
+	drawText("                               ███    ███              ", 9, 1, 18);
+	drawText("      Press 'p' to play", 17, 1, 19);
 }
 
 function drawMain()
@@ -167,13 +186,95 @@ function drawSell() { }
 
 function drawShop() { }
 
-function drawBank() { }
+function drawBank() {
+	clearScreen();
+	drawText("Welcome to Internation Bank.", 12, 1, 1);
+	drawText("Thank you for banking with us!", 12, 1, 2);
+	drawText("Customer Account: #2264854\/B29", 12, 1, 3);
+	drawText("Please select from these services:", 12, 1, 5);
+	drawText("(D) Deposit", 12, 1, 6);
+	drawText("(W) Withdrawal", 12, 1, 7);
+	drawText("Press 'b' to go back.", 12, 1, 9);
+}
 
-function drawLoan() { }
+function drawBankWithdrawl() {
 
+}
+
+function drawBankDeposit() {
+	clearScreen();
+	drawText("You have " + cash + " on hand.", 12, 1, 1);
+	drawText("How much do you wish to deposit?", 12, 1, 2);
+	drawText("Savings accrue 1.5% interest per day.", 12, 1, 3)
+	drawText("(A) 1000", 12, 1, 5);
+	drawText("(S) 5000", 12, 1, 6);
+	drawText("(D) 10000", 12, 1, 7);
+	drawText("(F) 30000", 12, 1, 8);
+	drawText("Press 'b' to return.", 12, 1, 10);
+}
+
+function drawLoan() {
+	clearScreen();
+	drawText("You owe " + debt + " to the loanshark.", 12, 1, 1);
+	drawText("How much do you wish to pay back?", 12, 1, 2);
+	drawText("(A) 1000", 12, 1, 4);
+	drawText("(S) 5000", 12, 1, 5);
+	drawText("(D) 10000", 12, 1, 6);
+	drawText("(F) 30000", 12, 1, 7);
+	debtCalc();
+	drawText("Press 'b' to return.", 12, 1, 10);
+}
+
+function bankCalc() {
+	if (bank > 0) {
+		bank = Math.floor(bank * 1.015);
+    }
+}
+
+function debtCalc() {
+	if ((cash - 1000) < 0) {
+		drawText("(A) 1000 (CAN'T AFFORD!)", 1, 1, 4);
+	}
+	else 
+		drawText("(A) 1000", 12, 1, 4);
+	if ((cash - 5000) < 0) {
+		drawText("(S) 5000 (CAN'T AFFORD!)", 1, 1, 5);
+	}
+	else
+		drawText("(S) 5000", 12, 1, 5);
+	if ((cash - 10000) < 0) {
+		drawText("(D) 10000 (CAN'T AFFORD!)", 1, 1, 6);
+	}
+	else
+		drawText("(D) 1000", 12, 1, 6);
+	if ((cash - 30000) < 0) {
+		drawText("(F) 30000 (CAN'T AFFORD!", 1, 1, 7);
+	}
+	else
+		drawText("(F) 30000", 12, 1, 7);
+	if ((cash - debt) < 0) {
+		drawText("(G) Everything (CAN'T AFFORD!)", 1, 1, 8);
+	}
+	else
+		drawText("(G) Everything (" + debt + ")", 12, 1, 8);
+}
 function drawClinic() { }
 
-function drawEnd() { }
+function drawEnd() {
+	clearScreen();
+	if (debt != 0) { endLoss(); }
+	else endWin();
+}
+
+function endWin() { }
+
+function endLoss() {
+	drawText("You failed to pay back all your debt!", 12, 1, 1);
+	drawText("The Loanshark kills you and takes all your money!", 12, 1, 2);
+	drawText("YOUR SCORE: 0", 12, 1, 3);
+	drawText("A score of 0 is not eligible for a high score entry!", 12, 1, 4)
+	drawText("Press 'r' to restart.", 12, 1, 6);
+}
 
 function rndPrice(min, max)
 {
@@ -191,48 +292,83 @@ function doDebt() {
 function onInput(key)
 {
 	switch (key) {
-		case 112:
+		case 97:
+			if (room = 8) {
+				cash = (cash - 1000);
+				bank = (bank + 1000);
+			}
+			break;
+		case 98: //b
+			if (room != 0) {
+				room = 0;
+			}
+			else if (room == 0) {
+				room = 4;
+            }
+			break;
+		case 100: //d
+			if (room == 4) {
+				room = 8;
+			}
+			break;
+		case 108: //l
+			room = 5;
+			break;
+		case 112: //p
 			if (room == -1) {
 				room++;
 				onUpdate();
 			}
 			break;
-		case 49:
+		case 114: //r
+			turn = 1;
+			debt = 5000;
+			heldProduct = [0, 0, 0, 0, 0, 0];
+			clearScreen();
+			room = -1;
+			onConnect();
+			break;
+		case 119: //w
+			if (room == 4) {
+				room = 9;
+			}
+			break;
+		case 49: //1
 			if (city != location[0]) {
 				city = location[0];
 				turn++;
 				setPrices();
 			}
 			break;
-		case 50:
+		case 50: //2
 			if (city != location[1]) {
 				city = location[1];
 				turn++;
 				setPrices();
 			}
 			break;
-		case 51:
+		case 51: //3
 			if (city != location[2]) {
 				city = location[2];
 				turn++;
 				setPrices();
 			}
 			break;
-		case 52:
+		case 52: //4
 			if (city != location[3]) {
 				city = location[3];
 				turn++;
 				setPrices();
 			}
 			break;
-		case 53:
+		case 53: //5
 			if (city != location[4]) {
 				city = location[4];
 				turn++;
 				setPrices();
 			}
 			break;
-		case 54:
+		case 54: //6
 			if (city != location[5]) {
 				city = location[5];
 				turn++;
